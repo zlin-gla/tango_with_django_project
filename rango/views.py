7b26ee3d38bc214699c72526c61e8d5056dddf81
Zachary Lin” <3116976L@student.gla.ac.uk>
2026-02-25 14:16:46 +0000

    added about page, tests pass

--- rango/views.py
+++ rango/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    about_page_url = '<a href="/rango/about/">About</a>'
+    return HttpResponse("Rango says hey there partner! " + about_page_url)
+
+def about(request):
+    home_page_url = '<a href="/rango/">Index</a>'
+    return HttpResponse("Rango says here is the about page. " + home_page_url)
 
