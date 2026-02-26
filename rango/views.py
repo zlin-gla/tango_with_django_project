1825b1ac9da75d434251ab24613059c2f220f00d
Zachary Lin” <3116976L@student.gla.ac.uk>
2026-02-26 08:12:25 +0000

    added in templates and images, tests pass

--- rango/views.py
+++ rango/views.py
@@ -2,10 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    about_page_url = '<a href="/rango/about/">About</a>'
-    return HttpResponse("Rango says hey there partner! " + about_page_url)
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    home_page_url = '<a href="/rango/">Index</a>'
-    return HttpResponse("Rango says here is the about page. " + home_page_url)
+    return render(request, 'rango/about.html')
 
