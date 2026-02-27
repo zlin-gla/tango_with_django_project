f62336333342bb9fce52983aa4a57ff37e68d15a
Zachary Lin” <3116976L@student.gla.ac.uk>
2026-02-27 08:42:23 +0000

    added population script, added columns in admin panel, tests pass

--- rango/admin.py
+++ rango/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.contrib import admin
 from rango.models import Category, Page
 
-admin.site.register(Category)
-admin.site.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
 
+admin.site.register(Category)
+admin.site.register(Page, PageAdmin)
--- rango/models.py
+++ rango/models.py
@@ -6,6 +6,9 @@ class Category(models.Model):
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
 
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     def __str__(self):
         return self.name
 
